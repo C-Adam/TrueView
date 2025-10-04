0b23b786b5d0f8339aa6789fb9344e8f3249c892
AravDe <dea@berea.edu>
2025-10-03 22:43:07 -0400

    Revert "Added file structure for the project"
    This reverts commit 1e5dec73becc5bd88fec75aa0ee3dde2438b2779.

--- backend/videoDetect.js
+++
@@ -1,9 +0,0 @@
-import { RealityDefender } from '@realitydefender/realitydefender';
-
-const realityDefender = new RealityDefender({
-  apiKey: 'your-api-key',
-});
-
-const result = await realityDefender.detect({
-  filePath: '/path/to/your/file.jpg',
-});
